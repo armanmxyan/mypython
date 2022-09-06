3d949cb9ebb2acc54047c68a2d48e8c131b40c47
armanmxyan <mmgmarman@gmail.com>
2022-09-06 20:16:23 +0400

    add circle.py

---
+++ circle.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+#----------------------------
+#
+# Autor Arman Mkhitaryan 
+# QUIZ | Python Day 1:
+#
+#---------------------------
+
+#--- variables -------------------
+
+_S=78.5
+_Pi=3.14
+_L=0
+_R=0
+
+#---- calculate Radius -----------------------------------
+
+_R=(_S/_Pi)**0.5               # R = ( S/Pi )^0.5
+print('Radius =',_R)
+
+#-----calculate Circle Lenght ---------------------------------
+
+_L=2*_Pi*_R                    # L = 2*Pi*R       
+print('Circle lenght =',_L)
+
+#-- End off code -------------------------------------
+
+
