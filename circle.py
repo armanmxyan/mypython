73c2ae141cea856d4ea7d6ae09b81d7d57cc53a6
armanmxyan <mmgmarman@gmail.com>
2022-09-07 13:15:16 +0400

    compare

--- circle.py
+++ circle.py
@@ -25,5 +25,4 @@ _L=2*_Pi*_R                    # L = 2*Pi*R
 print('Circle lenght =',_L)
 
 #-- End programm -------------------------------------
-#branch new logi
-#:vcnxmc,mvnxmc
+ifjkdslfjsdkfjj
